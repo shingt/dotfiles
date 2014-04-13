a5863d2fe63f3ae6a6686e5234ce9f8624acdae1
shin-en <van.s.ver@gmail.com>
2014-04-14 05:59:24 +0900

    Update setup file

--- setup.sh
+++ setup.sh
@@ -3,3 +3,9 @@
 for file in `find $HOME/dotfiles -name '.*' | grep -v 'dotfiles/.git$' | perl -nle 'm!dotfiles/(.+)$! and print $1'`; do
     ln -s $HOME/dotfiles/$file $HOME/$file
 done
+
+cd $HOME/dotfiles
+git submodule init
+git submodule update
+
+mkdir $HOME/backup
