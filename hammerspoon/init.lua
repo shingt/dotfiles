7124b40a935d71987df5ef048cf2363d52c5dcda
shingt <shingtshin@gmail.com>
2019-09-21 15:22:23 +0900

    Indentationg

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,41 +1,45 @@
 local function keyCode(key, modifiers)
-   modifiers = modifiers or {}
-   return function()
-      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), true):post()
-      hs.timer.usleep(1000)
-      hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), false):post()
-   end
+  modifiers = modifiers or {}
+  return function()
+    hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), true):post()
+    hs.timer.usleep(1000)
+    hs.eventtap.event.newKeyEvent(modifiers, string.lower(key), false):post()
+  end
 end
 
 local function remapKey(modifiers, key, keyCode)
-   hs.hotkey.bind(modifiers, key, keyCode, nil, keyCode)
+  hs.hotkey.bind(modifiers, key, keyCode, nil, keyCode)
 end
 
 local function disableAllHotkeys()
-   for k, v in pairs(hs.hotkey.getHotkeys()) do
-      v['_hk']:disable()
-   end
+  for k, v in pairs(hs.hotkey.getHotkeys()) do
+    v['_hk']:disable()
+  end
 end
 
 local function enableAllHotkeys()
-   for k, v in pairs(hs.hotkey.getHotkeys()) do
-      v['_hk']:enable()
-   end
+  for k, v in pairs(hs.hotkey.getHotkeys()) do
+    v['_hk']:enable()
+  end
 end
 
 local function handleGlobalAppEvent(name, event, app)
-   if event == hs.application.watcher.activated then
-      if (name ~= "iTerm2") and (name ~= "Xcode") and (name ~= "Google Chrome") and (name ~= "Code") and (name ~= "Inkdrop") then
-         enableAllHotkeys()
-      else
-         disableAllHotkeys()
-      end
-   end
+  if event == hs.application.watcher.activated then
+    if (name ~= "iTerm2")
+      and (name ~= "Xcode")
+      and (name ~= "Google Chrome")
+      and (name ~= "Code")
+      and (name ~= "Inkdrop") then
+      enableAllHotkeys()
+    else
+      disableAllHotkeys()
+    end
+  end
 end
 
 local function keyCtrlK()
-   keyCode('right', {'shift', 'cmd'})()
-   keyCode('x', {'cmd'})()
+  keyCode('right', {'shift', 'cmd'})()
+  keyCode('x', {'cmd'})()
 end
 
 appsWatcher = hs.application.watcher.new(handleGlobalAppEvent)
